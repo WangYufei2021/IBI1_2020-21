363d0983a39313f75e67bd4fad015021d0290482
WangYufei <yufeiw.20@intl.zju.edu.cn>
2021-03-10 16:55:20 +0800

    change the pseudocode of r_rate.py in Practical5 to actual code

--- Practical5/r_rate.py
+++ Practical5/r_rate.py
@@ -1,11 +1,16 @@
 # Define r rate as 1.2
+r = 1.2
 
 # Define the number at the start
+n = 84
 
 # Use for loop to circulate 5 times
+for i in range (1,6):
 
 # Each time 1.2 times of the number of the previous infectors will be added, that is n*r
 # The total number of infectors is n + n*r
+    n = n + n*r 
 
 # Output r and the number of infectors
 # r should be described as defined
+print("When r(the rate of reproduction of COVID-19) is ",r, ", ",  "The total number of individuals infected after 5 generations is ", str(n))
