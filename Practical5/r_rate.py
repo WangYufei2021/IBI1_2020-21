36ecdba21d760917ddcd5abbfd2c163bd1bcd53f
WangYufei <yufeiw.20@intl.zju.edu.cn>
2021-03-10 16:53:43 +0800

    add r_rate.py in Practical5

---
+++ Practical5/r_rate.py
@@ -0,0 +1,11 @@
+# Define r rate as 1.2
+
+# Define the number at the start
+
+# Use for loop to circulate 5 times
+
+# Each time 1.2 times of the number of the previous infectors will be added, that is n*r
+# The total number of infectors is n + n*r
+
+# Output r and the number of infectors
+# r should be described as defined
